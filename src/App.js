814a20bc38956822ae27f0fe3a69deef989a7964
Harman Batheja <66913564+harmanbatheja15@users.noreply.github.com>
2020-11-15 20:29:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
           <Navbar />
           {/* <Home /> */}
           <Switch>
-              <Route exact path="/" component={Home} />
+              <Route exact path="/K-Ride" component={Home} />
               <Route exact path="/about" component={About} />
               <Route exact path="/projects" component={Projects} />
               {/* <Route exact path="/annual-reports" component={} />
